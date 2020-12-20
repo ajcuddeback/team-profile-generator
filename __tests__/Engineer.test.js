92def86c20f3d5c832f53ed96627a58e5a1c144f
ajcudd <ajcuddeback@gmail.com>
2020-12-20 17:02:30 -0500

    added failing tests for engineer

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,1 +1,17 @@
+const { test, expect } = require("@jest/globals")
 const Engineer = require("../lib/Engineer")
+
+test('Creates enginineer object', () => {
+    const engineer = new Engineer('ajcuddeback')
+
+    expect(engineer.name).toBe('AJ');
+    expect(engineer.id).toBe('25');
+    expect(engineer.email).toBe('ajcuddeback@gmail.com');
+    expect(engineer.github).toBe('ajcuddeback');
+});
+
+test('gets role of engineer', () => {
+    const engineer = new Engineer('ajcuddeback')
+
+    expect(engineer.getRole()).toEqual(expect.any(String));
+});
